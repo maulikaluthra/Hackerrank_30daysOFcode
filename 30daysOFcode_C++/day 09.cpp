028b0e45d28cc60d740921e3cc31fc892fba31a3
maulikaluthra <56387512+maulikaluthra@users.noreply.github.com>
2020-10-20 19:03:37 +0530

    Update day 09.cpp
    cleaner code

--- 30daysOFcode_C++/day 09.cpp
+++ 30daysOFcode_C++/day 09.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 // Complete the factorial function below.
 int factorial(int n) {
-int fact=1;
-while(n>0){
-fact=fact*n;
-n--;
-} return fact;
+    int fact = 1;
+    while(n--)
+        fact*=n;
+    return fact;
 }
 
 int main()
